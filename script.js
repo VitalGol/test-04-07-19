858bb7b276495dd22e9594dd68c2f231b09635c1
Vitaly <vitalgolob@gmail.com>
2019-07-04 15:57:32 +0300

    123

---
+++ script.js
@@ -0,0 +1,20 @@
+// const arr = [1, 2, 3, 4, 5, 6];
+// arr.find(it => it % 2 === 0);
+// console.log(it + "</br>");
+// document.write(it+'</br>');
+// console.log(arr);
+// const a1 = "a1";
+// const a2 = "a2";
+// const a3 = "a3";
+// const a4 = "a4";
+// const arr = { a1, a2, a3, a4 };
+// console.log(arr);
+const person = {
+  name: "Ann",
+  age: 25,
+  greet: () => {
+    console.log("hello world");
+  }
+};
+const persons = new Object();
+console.log(persons);
