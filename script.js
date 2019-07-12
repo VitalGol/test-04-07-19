75f7f7288421413fcd1343bbb0606f0ddf39f9d5
Vitaly <vitalgolob@gmail.com>
2019-07-12 13:27:08 +0300

    async load

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 document.querySelector("#load").addEventListener("click", load);
 
-function load() {
+async function load() {
   let url = "https://jsonplaceholder.typicode.com/users";
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      let ul = document.querySelector("#list");
-      let html = data.map(
-        item => `<li> ${item.id} ${item.name} (${item.email})</li>`
-      );
-      ul.insertAdjacentHTML("afterbegin", html.join(" "));
-    });
+
+  // fetch(url)
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     let ul = document.querySelector("#list");
+  //     let html = data.map(
+  //       item => `<li> ${item.id} ${item.name} (${item.email})</li>`
+  //     );
+  //     ul.insertAdjacentHTML("afterbegin", html.join(" "));
+  //   });
+
+  let response = await fetch(url);
+  let data = await response.json();
+  let ul = document.querySelector("#list");
+  let html = data.map(
+    item => `<li> ${item.id} ${item.name} (${item.email})</li>`
+  );
+  ul.insertAdjacentHTML("afterbegin", html.join(" "));
 }
