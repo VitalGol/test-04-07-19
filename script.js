8d212cf5a0792a8b2a6f15f7a59da0cf460a98a8
Vitaly <vitalgolob@gmail.com>
2019-07-12 13:17:10 +0300

    fetch()

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const person = new Object({
-  name: "Ann",
-  age: 25,
-  greet: () => {
-    console.log("hello world");
-  }
-});
-console.log(person);
+document.querySelector("#load").addEventListener("click", load);
+
+function load() {
+  let url = "https://jsonplaceholder.typicode.com/users";
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      let ul = document.querySelector("#list");
+      let html = data.map(
+        item => `<li> ${item.id} ${item.name} (${item.email})</li>`
+      );
+      ul.insertAdjacentHTML("afterbegin", html.join(" "));
+    });
+}
